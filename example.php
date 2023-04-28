10051769889210b424f8656d5093ab44492b360f
Daniele Basile <daniele.basile.job@gmail.com>
2023-04-28 10:40:53 +0200

    declaring constants in php - two ways

--- example.php
+++ example.php
@@ -2,13 +2,19 @@
 //superglobal variables - derived from form - post 
 //echo 'Hello'.$_POST['name']. '' .$_POST['surname'];
 
-$name = $_POST['name'];
-echo $name;
+// $name = $_POST['name'];
+// echo $name;
 
-function greet(){
-    $GLOBALS['name'] ='Orazio';
-    echo $GLOBALS['name'];
-}
-greet();
-echo $name
+// function greet(){
+//     $GLOBALS['name'] ='Orazio';
+//     echo $GLOBALS['name'];
+// }
+// greet();
+// echo $name
+
+//DECLARING CONSTANTS
+define('WAY', 'via carrera');
+const ADDRESS = 'viale genova';
+echo WAY;
+echo ADDRESS;
 ?>
