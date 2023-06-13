c21ee47387016226807848f9602318ab048d9bcd
Daniele Basile <daniele.basile.job@gmail.com>
2023-06-13 13:00:08 +0200

    example showing how to open and read a txt file from php

--- example.php
+++ example.php
@@ -1,6 +1,14 @@
 <?php 
 
-$name = $_POST['name'];
-$surname = $_POST['surname'];
-echo $name .' '.$surname;
+$file = 'note.txt';
+if(file_exists($file)){
+    $ref = fopen($file,"r");
+
+    $content = fread($ref, filesize($file));
+    echo $content;
+    fclose($ref);
+}else{
+    echo 'file does not exists';
+};
+
 ?>
--- index.php
+++ index.php
@@ -6,39 +6,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Form handling</title>
-  <style>
-.form_style{
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  gap:0.8rem;
-  color:lightgrey;
-}
-#name, #surname{
-  width:250px;
-  border-radius:23px;
-}
-#btn_style{
-  width:180px;
-  border-radius:23px;
-}
-  </style>
+
 </head>
 <?php
 
 ?>
 
 <body>
-  <h1>Form handling in PHP</h1>
-  <form class="form_style" action="example.php" method="POST">
-    <label for="name">Name</label>
-    <input type="text" name="name" id="name" required>
-
-    <label for="surname">Surname</label>
-    <input type="text" name="surname" id="surname">
-
-    <input type="submit" id="btn_style" value="send module">
-  </form>
+  <h1>Working with files in PHP</h1>
+  
 </body>
 
 </html>
