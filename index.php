02d81b534dc8423ff27dea221a56e12c7ffc401c
Daniele Basile <daniele.basile.job@gmail.com>
2023-06-13 11:33:28 +0200

    split a text with quantifier and looped it, example

--- index.php
+++ index.php
@@ -8,18 +8,28 @@
   <title>Document</title>
 </head>
 <?php
-$firstPattern = '/Og[gk]i/'; // this syntax means it has to contain a word beginning with Og then it has to be one of the letter put in square brackedt and end in i
-$secondPattern = '/Og[^gk]i/'; // in this case it has not to contain one of the letter between the g or k
-$thirdPattern = '/Og[a-z]i/'; // in this case it has to conain a letter between a and z//that's considered a pattern
+//$firstPattern = '/Og[gk]i/'; // this syntax means it has to contain a word beginning with Og then it has to be one of the letter put in square brackedt and end in i
+//$secondPattern = '/Og[^gk]i/'; // in this case it has not to contain one of the letter between the g or k
+//$thirdPattern = '/Og[a-z]i/'; // in this case it has to conain a letter between a and z//that's considered a pattern
 //[A-Z] letter from a to z uppercase [a-Z] - letters from a to z both lowercase or uppercase [0-9] - numbers from 0 to 9
 
-$text = 'Oggi è bel tempo';
+//predefined class
+// $text = 'Oggi è bel tempo';
 
-$imgName = 'firm logo';
-$spacePatternCheck = '/\s/';
-$replacement = '_';
 
-echo preg_replace($spacePatternCheck, $replacement, $imgName);
+// $imgName = 'firm logo';
+// $spacePatternCheck = '/\s/';
+// $replacement = '_';
+
+//echo preg_replace($spacePatternCheck, $replacement, $imgName);
+
+$pattern = '/[\s,]+/';
+$text = 'My favourite colors are red, green and blue';
+$parts = preg_split($pattern, $text);
+
+foreach($parts as $part){
+  echo $part . '</br>';
+};
 ?>
 
 <body>
