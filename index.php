301e89b662f95fd7cefe4c01d675f8fb49d6ca07
Daniele Basile <daniele.basile.job@gmail.com>
2023-06-13 12:05:47 +0200

    first example to retrieve form data in php - get request

--- index.php
+++ index.php
@@ -5,49 +5,40 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Form handling</title>
+  <style>
+.form_style{
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  gap:0.8rem;
+  color:lightgrey;
+}
+#name, #surname{
+  width:250px;
+  border-radius:23px;
+}
+#btn_style{
+  width:180px;
+  border-radius:23px;
+}
+  </style>
 </head>
 <?php
-//$firstPattern = '/Og[gk]i/'; // this syntax means it has to contain a word beginning with Og then it has to be one of the letter put in square brackedt and end in i
-//$secondPattern = '/Og[^gk]i/'; // in this case it has not to contain one of the letter between the g or k
-//$thirdPattern = '/Og[a-z]i/'; // in this case it has to conain a letter between a and z//that's considered a pattern
-//[A-Z] letter from a to z uppercase [a-Z] - letters from a to z both lowercase or uppercase [0-9] - numbers from 0 to 9
 
-//predefined class
-// $text = 'Oggi è bel tempo';
-
-
-// $imgName = 'firm logo';
-// $spacePatternCheck = '/\s/';
-// $replacement = '_';
-
-//echo preg_replace($spacePatternCheck, $replacement, $imgName);
-
-// $pattern = '/[\s,]+/';
-// $text = 'My favourite colors are red, green and blue';
-// $parts = preg_split($pattern, $text);
-
-// foreach($parts as $part){
-//   echo $part . '</br>';
-// };
-
-// $pattern = "/^M/";
-// $names = array('Marco Rossi', 'Luca Verdi', 'Mattia Gialli');
-// $matches = preg_grep($pattern, $names);
-
-// foreach($matches as $match){
-//   echo $match . '</br>';
-// };
-
-$pattern = '/\bcar\w*/';
-$replacement = '<b>$0</b>';
-$text = 'Words beginning with car: cars, cart, carrot, cartoon. Words ending with car: scar, supercar.';
-
-echo preg_replace($pattern, $replacement, $text);
 ?>
 
 <body>
-  <h1>Learning regex in PHP</h1>
+  <h1>Form handling in PHP</h1>
+  <form class="form_style" action="example.php" method="GET">
+    <label for="name">Name</label>
+    <input type="text" name="name" id="name">
+
+    <label for="surname">Surname</label>
+    <input type="text" name="surname" id="surname">
+
+    <input type="submit" id="btn_style" value="send module">
+  </form>
 </body>
 
 </html>
